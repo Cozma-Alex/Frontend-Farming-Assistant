2fc469f40089e4cd0755166db86afd8cd1275e6f
Daniel <danutlechea2003@yahoo.com>
2024-11-12 17:34:00 +0200

    FA-5: fullscreen mode?

--- farming_assistant/lib/main.dart
+++ farming_assistant/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'screens/login_screen.dart';
 
 void main() {
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const MyApp());
 }
 
