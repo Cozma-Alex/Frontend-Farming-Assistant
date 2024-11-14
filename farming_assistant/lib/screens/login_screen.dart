ff1c0351a4634ca117c9f47bba1fb5eef27306e9
Costea Gabriel-Antonio <81188258+AXXPRO@users.noreply.github.com>
2024-11-14 15:03:29 +0200

    Login api implementation (#7)
    * FA-16: Implemented basic login API call
    
    * FA-16: Added clarification comments to a class
    
    * FA-16: Newlines at the end of files

--- farming_assistant/lib/screens/login_screen.dart
+++ farming_assistant/lib/screens/login_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:farming_assistant/widgets/register_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../models/user.dart';
+import  '../utils/config.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -9,6 +14,10 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   void _openRegisterWidget() {
     showModalBottomSheet(
       isScrollControlled: true,
@@ -101,6 +110,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                   const SizedBox(height: 20),
                   TextField(
+                    controller : emailController,
                     decoration: InputDecoration(
                       hintText: 'Email...',
                       hintStyle: Theme.of(context)
@@ -117,6 +127,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                   const SizedBox(height: 15),
                   TextField(
+                    controller : passwordController,
                     obscureText: true,
                     obscuringCharacter: '*',
                     decoration: InputDecoration(
@@ -172,7 +183,21 @@ class _LoginScreenState extends State<LoginScreen> {
                   SizedBox(
                     width: 220,
                     child: ElevatedButton(
-                      onPressed: () {},
+                      //Basic login API call.
+                      //TODO: Actual implementation of page navigation and display failed login
+                      onPressed: () {
+                        String username = emailController.text;
+                        String password = passwordController.text;
+                        var userData = loginAPI(username, password);
+                        userData.then((value) {
+                          if (value.statusCode == 200) {
+                            var jsonData = jsonDecode(value.body);
+                            var user = User.fromJson(jsonData);
+                          } else {
+                            print('${value.statusCode} - error');
+                          }
+                        });
+                      },
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Theme.of(context).colorScheme.primary,
                         shape: RoundedRectangleBorder(
@@ -218,5 +243,26 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
     );
+  }
+}
+
+Future<http.Response> loginAPI(String email, String password) async {
+  final uri = Uri.parse('${APIConfig.baseURI}/users/auth');
+
+  try {
+    final response = await http.post(
+      uri,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'email': email,
+        'password_hash': password,
+      }),
+    );
+
+    return response;
+  } catch (e) {
+    throw Exception('Failed to login: $e');
   }
 }
