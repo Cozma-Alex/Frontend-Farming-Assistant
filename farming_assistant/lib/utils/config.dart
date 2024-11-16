1af560af5ae3ddaf3109dde005107ce825ba5945
AXXPRO <antoniogabi03@yahoo.com>
2024-11-16 19:58:58 +0200

    FA-20: Use of hostname instead of ip

--- farming_assistant/lib/utils/config.dart
+++ farming_assistant/lib/utils/config.dart
@@ -1,10 +1,32 @@
-class APIConfig {
+import 'dart:io';
 
+class APIConfig {
   //For testing purposes, this is the IP address of the machine running the backend server on your local network
   //Likely to be changed to an actual domain name in the end
   //!-Change to your own IP address-!
-  static const String ip = '192.168.0.47';
-  static const String port = '8080';
-  static const String baseURI = 'http://$ip:$port/api';
+  static const String hostname = 'axxpro360.tplinkdns.com';
+  static String ip = '192.168.0.47';
+  static const String port = '6666';
+  static String baseURI = 'http://$ip:$port/api';
+
+  APIConfig(){
+      if(hostname != '') {
+        resolveHostname();
+      }
+  }
+
+  void resolveHostname() async {
+    if(hostname != ''){
+      try {
+        List<InternetAddress> addresses = await InternetAddress.lookup(hostname);
+        for (var address in addresses) {
+          ip = address.address;
+        }
+      } catch (e) {
+        throw Exception('Failed to resolve hostname: $e');
+      }
+    }
+  }
+
 
 }
